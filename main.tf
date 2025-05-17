dd091541616b981ec03bfa00543299f12df1a453
kunal7703 <143079753+kunal7703@users.noreply.github.com>
2025-05-17 11:59:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ required_version= "~> 1.1"
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+access_key = var.access_key
+secrect_key = var.secrect_key
 }
 resource "aws_instance" "myec2" {
   ami                     = "ami-084568db4383264d4"
